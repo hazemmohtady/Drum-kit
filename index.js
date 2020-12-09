9f863a1b7b038de00174636552b3f1f5874d3c02
hazem mohtady <hazem.mohtady93@gmail.com>
2020-12-09 10:35:51 +0200

    commit

---
+++ index.js
@@ -0,0 +1,73 @@
+
+for (var i=0;i<document.querySelectorAll(".drum").length;i++){
+document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+playAudio(this.textContent);
+buttonAnimation(this.textContent);
+
+});
+
+
+
+
+}
+
+
+
+document.addEventListener("keydown",function(event){
+  playAudio(event.key)
+  buttonAnimation(event.key)
+});
+
+
+
+
+
+function playAudio(key){
+  switch (key) {
+    case "w":
+    var audio = new Audio(["sounds/tom-1.mp3"]);
+    audio.play();
+      break;
+
+      case "a":
+      var audio = new Audio(["sounds/tom-2.mp3"]);
+      audio.play();
+        break;
+
+        case "s":
+        var audio = new Audio(["sounds/tom-3.mp3"]);
+        audio.play();
+          break;
+
+          case "d":
+          var audio = new Audio(["sounds/tom-4.mp3"]);
+          audio.play();
+            break;
+
+            case "j":
+            var audio = new Audio(["sounds/snare.mp3"]);
+            audio.play();
+              break;
+
+              case "k":
+              var audio = new Audio(["sounds/crash.mp3"]);
+              audio.play();
+                break;
+
+                case "l":
+                var audio = new Audio(["sounds/kick-bass.mp3"]);
+                audio.play();
+                  break;
+
+                default:
+
+  }
+}
+
+
+function buttonAnimation(key){
+ document.querySelector("." + key).classList.add("pressed");
+setTimeout(function(){
+  document.querySelector("." + key).classList.remove("pressed");
+}, 100);
+}
